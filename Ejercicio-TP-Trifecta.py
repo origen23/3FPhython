4ef856f6efaca7d4aed1dd9f8abd52f9f7fc2060
origen23 <127496525+origen23@users.noreply.github.com>
2024-05-24 23:55:16 -0300

    Add files via upload
    Entrega de TP Trifecta

---
+++ Ejercicio-TP-Trifecta.py
@@ -0,0 +1,46 @@
+# Ejercicio TP
+import random
+print("La Trifecta")
+
+numero_ingresado = 0
+palabra_ingresada = 0
+cantidad_palabra = 0
+es_mayor = True
+es_num   = True
+
+
+while es_mayor and es_num:
+    numero_ingresado = input(f"ingrese un numero mayor a cero:")
+    es_num = numero_ingresado.isdigit()
+    if es_num:
+       if numero_ingresado < 1:
+           es_mayor = False
+       else:
+            print("¡¡¡ F I N del ciclo !!! ")    
+    else:
+        print("¡¡¡ F I N del ciclo !!! ")    
+                    
+
+    if es_mayor and es_num:
+        palabra_ingresada = input(f"ingrese una palabra o frase:")
+        cantidad_palabra = len(palabra_ingresada)
+        print(f"La palabra ingresada tiene {cantidad_palabra} caracteres ") 
+
+
+    
+    
+    # if numero_ingresado == numero_secreto:
+    #     print(f"¡¡¡ CORRECTO !!! Numeros iguales ... ¡¡¡ Ha ganado el juego !!!")
+    #     print("¡¡¡ F I N !!! ")
+    #     sigo = False
+    # elif numero_ingresado < numero_secreto:
+    #     print(f"¡¡¡ ERROR !!! El número ingresado es menor al numero secreto")
+    # else:
+    #     print(f"¡¡¡ ERROR !!! El número ingresado es mayor al numero secreto")
+
+    # if contador == 5 and sigo:
+    #     print(f"¡¡¡ FIN DEL JUEGO !!!  No hay mas intentos - Lamentablemente ha perdido")
+    #     print(f"¡¡¡ EL NUMERO SECRETO ES EL {numero_secreto} ")
+    # elif sigo:
+    #     print(f"Tiene {(intentos-contador)} intentos mas ")
+    
