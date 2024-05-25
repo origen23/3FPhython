5fe9f4e780e3360ba2c1fc78d3122aed1268e9bb
origen23 <127496525+origen23@users.noreply.github.com>
2024-05-25 12:01:10 -0300

    Add files via upload

--- Ejercicio-TP-Trifecta.py
+++ Ejercicio-TP-Trifecta.py
@@ -5,18 +5,19 @@ print("La Trifecta")
 numero_ingresado = 0
 palabra_ingresada = 0
 cantidad_palabra = 0
+num_llave = 0
 es_mayor = True
 es_num   = True
-
+factorial = 0
 
 while es_mayor and es_num:
-    numero_ingresado = input(f"ingrese un numero mayor a cero:")
+    numero_ingresado = input(f"ingrese un numero mayor a cero para continuar:")
     es_num = numero_ingresado.isdigit()
     if es_num:
-       if numero_ingresado < 1:
+       numero_llave = int(numero_ingresado)
+       if not (numero_llave) > 0:
            es_mayor = False
-       else:
-            print("¡¡¡ F I N del ciclo !!! ")    
+           print("¡¡¡ F I N del ciclo !!! ")                       
     else:
         print("¡¡¡ F I N del ciclo !!! ")    
                     
@@ -24,7 +25,28 @@ while es_mayor and es_num:
     if es_mayor and es_num:
         palabra_ingresada = input(f"ingrese una palabra o frase:")
         cantidad_palabra = len(palabra_ingresada)
+        print(f"La palabra ingresada es ' {palabra_ingresada} ' ") 
         print(f"La palabra ingresada tiene {cantidad_palabra} caracteres ") 
+
+        factorial=1
+        for i in range (1,(cantidad_palabra+1),1):
+                factorial = i*factorial
+
+        print(f"El Factorial de  {cantidad_palabra} es {factorial} ")             
+
+        es_par = False
+        num_aux = 0
+
+        num_aux = factorial / 2
+        if int(num_aux) * 2 == factorial:
+             es_par = True
+             print(f"El Factorial {factorial} es par ")             
+        else:
+             print(f"El Factorial {factorial} es impar ")             
+                  
+
+
+             
 
 
     
